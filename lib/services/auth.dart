992d46946bd6f57679e25d0dd5c7df62630d018a
Aus Gomez <austrebertog@gmail.com>
2020-03-04 19:33:23 -0800

    finishing part 4

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,7 +1,20 @@
 import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthService {
+
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
   // sign in anom
+  Future signInAnom() async {
+    try {
+      AuthResult result = await _auth.signInAnonymously();
+      FirebaseUser user = result.user;
+      return user;
+    }catch(e){
+      print(e.toString());
+      return null;
+    }
+  }
 
   // sign in with email and password
 
